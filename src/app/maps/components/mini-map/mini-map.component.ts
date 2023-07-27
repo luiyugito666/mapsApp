a46700a48d0fd8312c566a35d0a5ac34957ee0f0
luiyugito666 <yugito_is@hotmail.com>
2023-07-26 19:59:28 -0500

    Update mini-map.component.ts
    refact MIniMapComponent

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
-  styleUrls: ['./mini-map.component.css']
+  
 })
 export class MiniMapComponent {
 
